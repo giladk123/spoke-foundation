0b464b2bbb06df7214a96dfe7ff2d0dc65fdfbc4
giladk123 <giladk@gmail.com>
2024-05-30 17:02:30 +0300

    linked the main.tf to the module

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 locals {
   data = jsondecode(file("./cac/rg.json"))
 }
-module "resource_group" {
-
-  source = "./module/rg"
+module "foundation" {
+  source  = "app.terraform.io/hcta-azure-dev/foundation/azurerm"
+  version = "1.0.0"
 
   resource_groups = local.data.resource_groups
   subscription_id = local.data.subscription.id
