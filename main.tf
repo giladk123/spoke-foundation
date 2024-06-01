ed0edc7c8cc70a8603679886e8bb586351211d61
giladk123 <giladk@gmail.com>
2024-06-01 14:38:07 +0300

    fix error in locals

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
-  data          = jsondecode(file("./foundation/rg.json"))
-  vnet_settings = jsondecode(file("./network/network.json"))
+  data                    = jsondecode(file("./ccoe/rg.json"))
+  vnet_settings           = jsondecode(file("./network/network.json"))
   log_analytics_workspace = jsondecode(file("./ccoe/log_analytics_workspace.json"))
 }
 module "foundation" {
@@ -11,8 +11,8 @@ module "foundation" {
 }
 
 module "vnet" {
-  source  = "app.terraform.io/hcta-azure-dev/vnet/azurerm"
-  version = "3.0.2"
+  source   = "app.terraform.io/hcta-azure-dev/vnet/azurerm"
+  version  = "3.0.2"
   for_each = { for k, v in local.vnet_settings : k => v }
 
   vnet_name      = each.value.vnet_name
@@ -30,15 +30,15 @@ module "log_analytics_workspace" {
   source  = "app.terraform.io/hcta-azure-dev/log_analytics_workspace/azurerm"
   version = "1.0.0"
 
-  law_name                    = local.log_analytics_workspace.law_name
-  rg_location                 = lookup(module.foundation.rg_location, "${local.log_analytics_workspace.location}", "")
-  rg_name                     = lookup(module.foundation.rg_name, "${local.log_analytics_workspace.resource_group_name}", "")
-  sku                         = local.log_analytics_workspace.sku
-  retention_in_days           = local.log_analytics_workspace.retention_in_days
-  internet_ingestion_enabled  = local.log_analytics_workspace.internet_ingestion_enabled
-  internet_query_enabled      = local.log_analytics_workspace.internet_query_enabled
-  tags                        = local.log_analytics_workspace.tags
+  law_name                   = local.log_analytics_workspace.law_name
+  rg_location                = lookup(module.foundation.rg_location, "${local.log_analytics_workspace.location}", "")
+  rg_name                    = lookup(module.foundation.rg_name, "${local.log_analytics_workspace.resource_group_name}", "")
+  sku                        = local.log_analytics_workspace.sku
+  retention_in_days          = local.log_analytics_workspace.retention_in_days
+  internet_ingestion_enabled = local.log_analytics_workspace.internet_ingestion_enabled
+  internet_query_enabled     = local.log_analytics_workspace.internet_query_enabled
+  tags                       = local.log_analytics_workspace.tags
 
   depends_on = [module.foundation]
-  
+
 }
