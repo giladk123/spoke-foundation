8654e46fc66b241c61a21f7b950d2e19b210737f
giladk123 <giladk@gmail.com>
2024-06-01 14:34:49 +0300

    fix error in module name

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ module "log_analytics_workspace" {
   version = "1.0.0"
 
   law_name                    = local.log_analytics_workspace.law_name
-  rg_location                 = lookup(module.resource_group.rg_location, "${local.log_analytics_workspace.location}", "")
-  rg_name                     = lookup(module.resource_group.rg_name, "${local.log_analytics_workspace.resource_group_name}", "")
+  rg_location                 = lookup(module.foundation.rg_location, "${local.log_analytics_workspace.location}", "")
+  rg_name                     = lookup(module.foundation.rg_name, "${local.log_analytics_workspace.resource_group_name}", "")
   sku                         = local.log_analytics_workspace.sku
   retention_in_days           = local.log_analytics_workspace.retention_in_days
   internet_ingestion_enabled  = local.log_analytics_workspace.internet_ingestion_enabled
