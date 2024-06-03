f54e1743c400e6a5f9805774da2f769e64901542
giladk123 <giladk@gmail.com>
2024-06-03 15:52:21 +0300

    changed network.json

--- outputs.tf
+++ outputs.tf
@@ -4,20 +4,35 @@ output "all_resource_groups" {
   description = "All resource groups created by the resource_group module"
 }
 
-output "vnet_id" {
-  value       = try(module.vnet.vnet.vnet_id, null)
-  description = "value of the vnet_id output from the vnet module"
+output "vnet_ids" {
+  value       = [for vnet in values(module.vnet) : vnet.vnet_id]
+  description = "The IDs of the Virtual Networks created by the vnet module."
 }
 
-output "vnet_name" {
-  value       = try(module.vnet.vnet.vnet_name, null)
-  description = "value of the vnet_name output from the vnet module"
+output "vnet_names" {
+  value       = [for vnet in values(module.vnet) : vnet.vnet_name]
+  description = "The names of the Virtual Networks created by the vnet module."
 }
 
 output "subnet_ids" {
-  value       = try(module.vnet.vnet.subnet_ids, null)
-  description = "value of the subnet_ids output from the vnet module"
+  value       = [for vnet in values(module.vnet) : vnet.subnet_ids]
+  description = "The IDs of the subnets within the Virtual Networks created by the vnet module."
 }
+
+# output "vnet_id" {
+#   value       = try(module.vnet.vnet.vnet_id, null)
+#   description = "value of the vnet_id output from the vnet module"
+# }
+
+# output "vnet_name" {
+#   value       = try(module.vnet.vnet.vnet_name, null)
+#   description = "value of the vnet_name output from the vnet module"
+# }
+
+# output "subnet_ids" {
+#   value       = try(module.vnet.vnet.subnet_ids, null)
+#   description = "value of the subnet_ids output from the vnet module"
+# }
 
 # output "all_resource_groups" {
 #   value       = module.rg
